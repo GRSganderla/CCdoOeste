c4fb7ce04c56b92aeda7cfecb14333182ef1287e
GRSganderla <grodriguessganderla@gmail.com>
2019-09-25 16:10:29 +0000

    arrumado

--- Appet/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './src/controller/menu';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
